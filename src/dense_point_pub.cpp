707e1f29c6ef1ab52276efe1230decd31b982dab
yshry <yshry.dev@gmail.com>
2019-06-22 01:01:27 +0900

    commit

--- src/dense_point_pub.cpp
+++ src/dense_point_pub.cpp
@@ -23,7 +23,8 @@ class DensePointPub
     DensePointPub(ros::NodeHandle& nh):__it(nh)
     {
         //this->__nh = nh;
-        this->__image_sub = this->__it.subscribe("/loam/camera_color_left/image_depth", 10, &DensePointPub::__imageCallback, this);
+        this->__image_depth_sub = this->__it.subscribe("/loam/camera_color_left/image_depth", 10, &DensePointPub::__imageDepthCallback, this);
+        this->__image_raw_sub = this->__it.subscribe("/loam/camera_color_left/image_raw", 10, &DensePointPub::__imageRawCallback, this);
         //this->__cam_info_sub = nh.subscribe("/loam/camera_color_left/camera_info", 10, &DensePointPub::__camInfoCallback, this);
         this->__point_cloud_pub = nh.advertise<sensor_msgs::PointCloud2>("/dense_depth", 10);
 
@@ -39,6 +40,7 @@ class DensePointPub
 		{
 			ros::spinOnce();
 			status = ros::ok();
+            this->__process();
 			rate.sleep();
 		}
 	}
@@ -46,37 +48,104 @@ class DensePointPub
     private:
     //ros::NodeHandle __nh;
     image_transport::ImageTransport __it;
-    image_transport::Subscriber __image_sub;
+    image_transport::Subscriber __image_depth_sub;
+    image_transport::Subscriber __image_raw_sub;
     ros::Subscriber __cam_info_sub;
     ros::Publisher __point_cloud_pub;
     Eigen::Matrix3f __intrinsics;
     Eigen::Matrix3f __inv_intrinsics;
-    const std::string __frame_id = "/camera_init";
+    std::string __frame_id;
     //tf2_ros::Buffer __tfBuffer;
     tf::TransformListener __tfListener;
+    cv::Mat __image_depth;
+    cv::Mat __image_raw;
+    ros::Time __image_depth_stamp;
+    ros::Time __image_raw_stamp;
+    bool __new_depth = false;
+    bool __new_raw = false;
 
-
-
-    void __imageCallback(const sensor_msgs::ImageConstPtr& msg)
+    void __imageDepthCallback(const sensor_msgs::ImageConstPtr& msg)
     {
-        //std::vector<uint16_t> casted_data (msg->data.begin(), msg->data.end());
-        //const uint16_t* casted_data = reinterpret_cast<const uint16_t*> (msg->data.data());
+        this->__image_depth = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::MONO16)->image;
+        this->__image_depth_stamp = msg->header.stamp;
+        this->__new_depth = true;
+        this->__frame_id = msg->header.frame_id;
+    }
 
+    void __imageRawCallback(const sensor_msgs::ImageConstPtr& msg)
+    {
+        this->__image_raw = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::RGB8)->image;
+        this->__image_raw_stamp = msg->header.stamp;
+        this->__new_raw = true;
+    }
+
+    void __process()
+    {
+        if (!(this->__new_depth && this->__new_raw))
+            return;
+        
+        ROS_INFO("new depth and new raw");
+
+
+        if (fabs((this->__image_depth_stamp - this->__image_raw_stamp).toSec()) < 0.05)
+            return;
+        ROS_INFO("stamp matched");
+        
+        pcl::PointCloud<pcl::PointXYZRGB> cloud;
+        int num_pixels = this->__image_depth.rows * this->__image_depth.cols;
+        cloud.width = num_pixels;
+        cloud.height = 1;
+        cloud.is_dense = true;
+        cloud.points.resize(num_pixels);
+
+        int num_points = 0;
+
+        for (int y=0; y<this->__image_depth.rows; y++)
+        {
+            for (int x=0; x<this->__image_depth.cols; x++)
+            {
+                uint16_t depth = this->__image_depth.at<uint16_t>(y,x);
+                cv::Vec3b color = this->__image_raw.at<cv::Vec3b>(y,x);
+                //if (depth >0 && depth < 10*256)
+                if (depth >0 && depth < 10*256)
+                {
+                    Eigen::Vector3f p = (float)(depth / 256.0) * this->__inv_intrinsics * Eigen::Vector3f(x,y,1);
+                    cloud[num_points].x = p(0);
+                    cloud[num_points].y = p(1);
+                    cloud[num_points].z = p(2);
+                    cloud[num_points].r = color(2);
+                    cloud[num_points].g = color(1);
+                    cloud[num_points].b = color(0);
+                    num_points++;
+                }
+               
+            }
+        }
+        cloud.points.resize(num_points);
+        cloud.width = num_points;
+
+        geometry_msgs::TransformStamped transform_stamped;
+        sensor_msgs::PointCloud2 point_msg;
+        pcl::toROSMsg(cloud, point_msg);
+        point_msg.header.stamp = this->__image_depth_stamp;
+        point_msg.header.frame_id = this->__frame_id;
+ 
+        this->__point_cloud_pub.publish(point_msg);
+        this->__new_depth = false;
+        this->__new_raw = false;
+    }
+
+    void __imageCallbackOld(const sensor_msgs::ImageConstPtr& msg)
+    {
         const cv::Mat casted_data = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::MONO16)->image;
         pcl::PointCloud<pcl::PointXYZRGB> cloud;
-        //pcl::PointCloud<pcl::PointXYZ> cloud_out;
         int num_pixels = casted_data.rows * casted_data.cols;
         cloud.width = num_pixels;
         cloud.height = 1;
         cloud.is_dense = true;
         cloud.points.resize(num_pixels);
 
-        //std::cout << num_pixels <<", " << msg->width * msg->height << std::endl;
-
         int num_points = 0;
-
-        sensor_msgs::ImageConstPtr color_img_msg = ros::topic::waitForMessage<sensor_msgs::Image>("/loam/camera_color_left/image_raw");
-
 
         for (int y=0; y<casted_data.rows; y++)
         {
@@ -87,13 +156,9 @@ class DensePointPub
                 if (depth >0)
                 {
                     Eigen::Vector3f p = (float)(depth / 256.0) * this->__inv_intrinsics * Eigen::Vector3f(x,y,1);
-                    //Eigen::Vector3f p = this->__inv_intrinsics * Eigen::Vector3f(x,y,1);
                     cloud[num_points].x = p(0);
                     cloud[num_points].y = p(1);
                     cloud[num_points].z = p(2);
-                    //cloud[num_points].x = ((float)x - this->__intrinsics(0,2))/ this->__intrinsics(0,0);
-                    //cloud[num_points].y = ((float)y  - this->__intrinsics(1,2)) / this->__intrinsics(1,1);
-                    //cloud[num_points].z = 1.0;
                     num_points++;
                 }
                
@@ -102,46 +167,13 @@ class DensePointPub
         cloud.points.resize(num_points);
         cloud.width = num_points;
 
-        //tf2::Stamped<tf2::Transform> point_transform_stamped;
-        //tf::Transform point_transform;
-        //tf::Transform point_transform_stamped;
         geometry_msgs::TransformStamped transform_stamped;
-        //try{
-            //transform_stamped = this->__tfBuffer.lookupTransform("loam_camera_color_left", "camera_init", msg->header.stamp);
-            //tf2::fromMsg(tmp_pt, point_transform_stamped);
-            //point_transform = point_transform_stamped.transform;
-        //}
-        //catch (tf2::TransformException &ex)
-        //{
-            //ROS_WARN("%s",ex.what());
-            //return;
-        //}
-
-        //pcl_ros::transformPointCloud("/camera_init", point_transform_stamped, cloud, cloud_out);
-        //pcl_ros::transformPointCloud(cloud, cloud_out, point_transform_stamped);
-
-        sensor_msgs::PointCloud2 point_msg, point_msg_in;
-        pcl::toROSMsg(cloud, point_msg_in);
-        point_msg_in.header.stamp = msg->header.stamp;
-        point_msg_in.header.frame_id = msg->header.frame_id;
-        //tf2::doTransform(point_msg_in, point_msg, transform_stamped);
-        std::string target_frame = "camera_init";
-        //ros::Time target_time = msg->header.stamp;
-        //std::string fixed_frame = msg->header.frame_id;
-        __tfListener.waitForTransform(target_frame, point_msg_in.header.frame_id, point_msg_in.header.stamp, ros::Duration(1.0));
-
-        //pcl_ros::transformPointCloud(target_frame, target_time, cloud, fixed_frame, cloud_out, this->__tfListener )
-        pcl_ros::transformPointCloud(target_frame, point_msg_in, point_msg, this->__tfListener);
-
-        //pcl::toROSMsg(cloud_out, point_msg);
-        //point_msg.header.stamp = msg->header.stamp;
-        //point_msg.header.stamp = ros::Time::now();
-        //point_msg.header.frame_id = this->__frame_id;
-        //point_msg.header.frame_id = "/camera_init";
+        sensor_msgs::PointCloud2 point_msg;
+        pcl::toROSMsg(cloud, point_msg);
+        point_msg.header.stamp = msg->header.stamp;
+        point_msg.header.frame_id = msg->header.frame_id;
  
         this->__point_cloud_pub.publish(point_msg);
-
-
     }
     
     void __camInfoCallback(const sensor_msgs::CameraInfoConstPtr& msg)
